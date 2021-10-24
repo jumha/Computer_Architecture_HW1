d959b8191d9ddd28f2cce707346727fa49c139c9
jumha <e24076750@gs.ncku.edu.tw>
2021-10-24 14:10:36 +0800

    add some comment

--- matrix_diagonal_sum.c
+++ matrix_diagonal_sum.c
@@ -11,7 +11,7 @@ int main() {
   size_t n = sizeof(*input_matrix) / sizeof(int);
   int *mat[n];
   int i;
-  for (i = 0; i < n; ++i) {
+  for (i = 0; i < n; ++i) { //Record the address of each row
     mat[i] = input_matrix[i];
   }
   int sum = diagonalSum(mat, n, n);
@@ -21,10 +21,10 @@ int main() {
 
 int diagonalSum(int** mat, int matSize, int matColSize) {
   int i, sum = 0;
-  for (i = 0; i < matSize; ++i) {
+  for (i = 0; i < matSize; ++i) { //Sum the primary diagonal and secondary diagonal
     sum += mat[i][i] + mat[i][matSize - i - 1];
   }
-  if (matSize % 2) {
+  if (matSize % 2) { //Substrate the intersection of primary diagonal and secondary diagonal      
     sum -= mat[matSize / 2][matSize / 2];
   }
   return sum;
